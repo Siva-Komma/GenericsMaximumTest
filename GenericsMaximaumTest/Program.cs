e50e255f2be91c0b54e31023e671806a4310ff21
Siva-Komma <kommasivajyothi22@gmail.com>
2023-02-24 18:14:35 +0530

    Finding Maximum float value

--- GenericsMaximaumTest/Program.cs
+++ GenericsMaximaumTest/Program.cs
@@ -12,7 +12,7 @@ namespace GenericsMaximaumTest
         {
             Console.WriteLine("Welcome to generics");
             Console.WriteLine("1.MaxInteger");
-            Console.WriteLine("Choose Your Option\n");
+            Console.WriteLine("Choose Your Option\n2.MaxFloat");
             int Option = Convert.ToInt32(Console.ReadLine());
             switch(Option)
             {
@@ -20,6 +20,11 @@ namespace GenericsMaximaumTest
                     MaxInteger.MaxIntegerNumber(100, 20, 30);
                     MaxInteger.MaxIntegerNumber(20, 40, 30);
                     MaxInteger.MaxIntegerNumber(10, 20, 30);
+                    break;
+                case 2:
+                    FloatMax.MaxFloatNumber(34.5f, 20.45f, 30.63f);
+                    FloatMax.MaxFloatNumber(20.22f, 20.23f, 10.4f);
+                    FloatMax.MaxFloatNumber(10.34f, 20.34f, 30.54f);
                     break;
             }
             Console.ReadLine();
