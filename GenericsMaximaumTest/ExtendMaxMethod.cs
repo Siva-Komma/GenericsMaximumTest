4ebcc32e31d58810cb069b74d8c6e66704824170
Siva-Komma <kommasivajyothi22@gmail.com>
2023-02-24 22:12:55 +0530

    Extend the max method taking more parameters

---
+++ GenericsMaximaumTest/ExtendMaxMethod.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsMaximaumTest
+{
+    class ExtendMaxMethod<T> where T : IComparable
+    {
+        public T firstvalue, secondvalue, thirdvalue,fourthvalue;
+        public static T ExtendMaxValue(T firstvalue, T secondvalue, T thirdvalue,T fourthvalue)
+        {
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0 && firstvalue.CompareTo(fourthvalue) > 0 ||
+               firstvalue.CompareTo(secondvalue) >= 0 && firstvalue.CompareTo(thirdvalue) > 0 && firstvalue.CompareTo(fourthvalue) >= 0 ||
+               firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) >= 0 && firstvalue.CompareTo(fourthvalue) > 0 )
+            {
+                Console.WriteLine("First value is bigger than other three values");
+                return firstvalue;
+            }
+            if (secondvalue.CompareTo(thirdvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 && secondvalue.CompareTo(fourthvalue)>0 ||
+                secondvalue.CompareTo(thirdvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0 && secondvalue.CompareTo(fourthvalue) >= 0 ||
+                secondvalue.CompareTo(thirdvalue) > 0 && secondvalue.CompareTo(thirdvalue) >= 0 && secondvalue.CompareTo(fourthvalue) > 0)
+            {
+                Console.WriteLine("Second value is bigger than other three value");
+                return secondvalue;
+            }
+            if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 && thirdvalue.CompareTo(fourthvalue) > 0 ||
+                thirdvalue.CompareTo(firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0 && thirdvalue.CompareTo(fourthvalue) >= 0 ||
+                thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >= 0 && thirdvalue.CompareTo(fourthvalue) > 0)
+            {
+                Console.WriteLine("Third value is bigger than other three value");
+                return thirdvalue;
+            }
+            if (fourthvalue.CompareTo(firstvalue) > 0 && fourthvalue.CompareTo(secondvalue) > 0 && fourthvalue.CompareTo(thirdvalue) > 0 ||
+               fourthvalue.CompareTo(firstvalue) >= 0 && fourthvalue.CompareTo(secondvalue) > 0 && fourthvalue.CompareTo(thirdvalue) >= 0 ||
+               fourthvalue.CompareTo(firstvalue) > 0 && fourthvalue.CompareTo(secondvalue) >= 0 && fourthvalue.CompareTo(thirdvalue) > 0)
+            {
+                Console.WriteLine("Fourth value is bigger than other three value");
+                return fourthvalue;
+            }
+            return firstvalue;
+        }
+    }
+}
--- GenericsMaximaumTest/Program.cs
+++ GenericsMaximaumTest/Program.cs
@@ -11,7 +11,7 @@ namespace GenericsMaximaumTest
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to generics");
-            Console.WriteLine("1.MaxInteger\n2.MaxFloat\n3.MaxString\n4.Refactor1\n5.Refactor2");
+            Console.WriteLine("1.MaxInteger\n2.MaxFloat\n3.MaxString\n4.Refactor1\n5.Refactor2\n6.ExtendMaxMethod");
             Console.WriteLine("Choose Your Option");
             int Option = Convert.ToInt32(Console.ReadLine());
             switch(Option)
@@ -43,6 +43,11 @@ namespace GenericsMaximaumTest
                     refactor.NewTestCase();
                     refactor1.NewTestCase();
                     refactor2.NewTestCase();
+                    break;
+                case 6:
+                    ExtendMaxMethod<int>.ExtendMaxValue(700, 200, 300,500);
+                    ExtendMaxMethod<float>.ExtendMaxValue(10.4f, 1.4f, 56.3f,100.4f);
+                    ExtendMaxMethod<string>.ExtendMaxValue("Peach", "Banana", "Apple","Mango");
                     break;
             }
             Console.ReadLine();
