6a3ad527cfcd3ef048352ce2a17bad57c81555b1
Siva-Komma <kommasivajyothi22@gmail.com>
2023-02-24 18:01:11 +0530

    Calculating Maximum integer

---
+++ GenericsMaximaumTest/MaxInteger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsMaximaumTest
+{
+    class MaxInteger
+    { 
+          public static int MaxIntegerNumber(int firstvalue, int secondvalue, int thirdvalue)
+            {
+                if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0 ||
+                    firstvalue.CompareTo(secondvalue) >= 0 && firstvalue.CompareTo(thirdvalue) > 0 ||
+                    firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) >= 0)
+                {
+                    Console.WriteLine("First number is bigger than other two numbers");
+                    return firstvalue;
+                }
+                if (secondvalue.CompareTo(thirdvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                    secondvalue.CompareTo(thirdvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                    secondvalue.CompareTo(thirdvalue) > 0 && secondvalue.CompareTo(thirdvalue) >= 0)
+                {
+                    Console.WriteLine("Second number is bigger than other two numbers");
+                    return secondvalue;
+                }
+                if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+                    thirdvalue.CompareTo(firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+                    thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >= 0)
+                {
+                    Console.WriteLine("Third number is bigger than other two numbers");
+                    return thirdvalue;
+                }
+
+                return firstvalue;
+          }
+    }
+}
--- GenericsMaximaumTest/Program.cs
+++ GenericsMaximaumTest/Program.cs
@@ -11,6 +11,17 @@ namespace GenericsMaximaumTest
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to generics");
+            Console.WriteLine("1.MaxInteger");
+            Console.WriteLine("Choose Your Option\n");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch(Option)
+            {
+                case 1:
+                    MaxInteger.MaxIntegerNumber(100, 20, 30);
+                    MaxInteger.MaxIntegerNumber(20, 40, 30);
+                    MaxInteger.MaxIntegerNumber(10, 20, 30);
+                    break;
+            }
             Console.ReadLine();
         }
     }
